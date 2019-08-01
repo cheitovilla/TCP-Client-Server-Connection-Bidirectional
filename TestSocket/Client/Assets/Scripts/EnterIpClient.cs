4866d8f845e179f3d215e30ab6c2f0a98b65f249
Dime Cheo <cheitovilla@outlook.com>
2019-08-01 14:16:00 -0500

    Finished
    Done!

---
+++ TestSocket/Client/Assets/Scripts/EnterIpClient.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnterIpClient : MonoBehaviour
+{
+    public static EnterIpClient Instance;
+
+    public InputField ip;
+    public string DireccionIp;
+
+    private void Awake()
+    {
+       // Instance = this;
+
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }
+
+    public void GiveMeTheIp()
+    {
+        DireccionIp = ip.text;
+        SceneManager.LoadScene("SceneClient");
+    }
+}
---
+++ TestSocket/ServerSocket/Assets/Scripts/EnterIpServer.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class EnterIpServer : MonoBehaviour
+{
+    public static EnterIpServer Instance;
+
+    public InputField ip;
+    public string DireccionIp;
+
+    private void Awake()
+    {
+        // Instance = this;
+
+        if (Instance)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+            Instance = this;
+        }
+    }
+
+    public void GiveMeTheIp()
+    {
+        DireccionIp = ip.text;
+        SceneManager.LoadScene("SceneServer");
+    }
+}
